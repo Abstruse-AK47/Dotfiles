70f2b61a3864eef138bdd0a1022fb744022f117a
Abstruse-AK47 <ayan344kapil@gmail.com>
2024-12-22 02:35:55 +0530

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,29 @@
 vim.g.mapleader = " "
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 0
+
+function ToggleStatusLine()
+	if vim.o.laststatus == 0 then
+		vim.o.laststatus = 2
+	else
+		vim.o.laststatus = 0
+	end
+end
+
+-- Map a key to toggle the status line (e.g., <leader>tl)
+vim.api.nvim_set_keymap("n", "<leader>il", ":lua ToggleStatusLine()<CR>", { noremap = true, silent = true })
+
+function ToggleTabLine()
+	if vim.o.showtabline == 0 then
+		vim.o.showtabline = 2
+	else
+		vim.o.showtabline = 0
+	end
+end
+
+-- Map a key to toggle the tab bar (e.g., <leader>tt)
+vim.api.nvim_set_keymap("n", "<leader>it", ":lua ToggleTabLine()<CR>", { noremap = true, silent = true })
 
 require("lazy_nvim.lazy")
 require("lazy_nvim.core")
--- .config/nvim/lua/lazy_nvim/plugins/quarto.lua
+++ .config/nvim/lua/lazy_nvim/plugins/quarto.lua
@@ -1,82 +1,101 @@
 return {
-    {
-    'jpalardy/vim-slime',
-    init=function ()
-      vim.g.slime_last_channel = { nil }
-      -- will use `# %%` to define cells
-      vim.g.slime_cell_delimiter = '\\s*#\\s*%%'
-      vim.g.slime_paste_file = os.getenv("HOME") .. "/.slime_paste"
+	{
+		"jpalardy/vim-slime",
+		init = function()
+			vim.g.slime_last_channel = { nil }
+			-- will use `# %%` to define cells
+			vim.g.slime_cell_delimiter = "\\s*#\\s*%%"
+			vim.g.slime_paste_file = os.getenv("HOME") .. "/.slime_paste"
 
-      local function next_cell()
-        vim.fn.search(vim.g.slime_cell_delimiter)
-      end
+			local function next_cell()
+				vim.fn.search(vim.g.slime_cell_delimiter)
+			end
 
-      local function prev_cell()
-        vim.fn.search(vim.g.slime_cell_delimiter, "b")
-      end
+			local function prev_cell()
+				vim.fn.search(vim.g.slime_cell_delimiter, "b")
+			end
 
-      vim.keymap.set('n', '<leader>cd', vim.cmd.SlimeSend, { noremap = true, desc = 'send line to term' })
-      vim.keymap.set('n', '<leader>cv', vim.cmd.SlimeConfig, { noremap = true, desc = "Open slime configuration" })
-      vim.keymap.set('n', '<leader>cs', '<Plug>SlimeRegionSend', { noremap = true, desc = 'send line to tmux' })
-      vim.keymap.set('n', '<leader>cp', '<Plug>SlimeParagraphSend', { noremap = true, desc = "Send Paragraph with Slime" })
-      vim.keymap.set('n', '<leader>ck', prev_cell, { noremap = true, desc = "Search backward for slime cell delimiter" })
-      vim.keymap.set('n', '<leader>cj', next_cell, { noremap = true, desc = "Search forward for slime cell delimiter" })
-      vim.keymap.set('n', '<leader>cc', '<Plug>SlimeSendCell', { noremap = true, desc = "Send cell to slime" }) 
-      local slime_get_jobid = function()
-        local buffers = vim.api.nvim_list_bufs()
-        local terminal_buffers = { "Select terminal:\tjobid\tname", }
-        local name = ""
-        local jid = 1
-        local chosen_terminal = 1
+			vim.keymap.set("n", "<leader>cd", vim.cmd.SlimeSend, { noremap = true, desc = "send line to term" })
+			vim.keymap.set(
+				"n",
+				"<leader>cv",
+				vim.cmd.SlimeConfig,
+				{ noremap = true, desc = "Open slime configuration" }
+			)
+			vim.keymap.set("n", "<leader>cs", "<Plug>SlimeRegionSend", { noremap = true, desc = "send line to tmux" })
+			vim.keymap.set(
+				"n",
+				"<leader>cp",
+				"<Plug>SlimeParagraphSend",
+				{ noremap = true, desc = "Send Paragraph with Slime" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>ck",
+				prev_cell,
+				{ noremap = true, desc = "Search backward for slime cell delimiter" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>cj",
+				next_cell,
+				{ noremap = true, desc = "Search forward for slime cell delimiter" }
+			)
+			vim.keymap.set("n", "<leader>cc", "<Plug>SlimeSendCell", { noremap = true, desc = "Send cell to slime" })
+			local slime_get_jobid = function()
+				local buffers = vim.api.nvim_list_bufs()
+				local terminal_buffers = { "Select terminal:\tjobid\tname" }
+				local name = ""
+				local jid = 1
+				local chosen_terminal = 1
 
-        for _, buf in ipairs(buffers) do
-          if vim.bo[buf].buftype == 'terminal' then
-            jid = vim.api.nvim_buf_get_var(buf, 'terminal_job_id')
-            name = vim.api.nvim_buf_get_name(buf)
-            table.insert(terminal_buffers, jid .. "\t" .. name)
-          end
-        end
+				for _, buf in ipairs(buffers) do
+					if vim.bo[buf].buftype == "terminal" then
+						jid = vim.api.nvim_buf_get_var(buf, "terminal_job_id")
+						name = vim.api.nvim_buf_get_name(buf)
+						table.insert(terminal_buffers, jid .. "\t" .. name)
+					end
+				end
 
-        -- if there is more than one terminal, ask which one to use
-        if #terminal_buffers > 2 then
-          chosen_terminal = vim.fn.inputlist(terminal_buffers)
-        else
-          chosen_terminal = jid
-        end
+				-- if there is more than one terminal, ask which one to use
+				if #terminal_buffers > 2 then
+					chosen_terminal = vim.fn.inputlist(terminal_buffers)
+				else
+					chosen_terminal = jid
+				end
 
-        if chosen_terminal then
-          print("\n[slime] jobid chosen: ", chosen_terminal)
-          return chosen_terminal
-        else
-          print("No terminal found")
-        end
-      end
+				if chosen_terminal then
+					print("\n[slime] jobid chosen: ", chosen_terminal)
+					return chosen_terminal
+				else
+					print("No terminal found")
+				end
+			end
 
-      local function slime_use_tmux()
-        vim.g.slime_target = "tmux"
-        vim.g.slime_bracketed_paste = 1
-        vim.g.slime_python_ipython = 0
-        vim.g.slime_no_mappings = 1
-        vim.g.slime_default_config = { socket_name = "default", target_pane = ":.2" }
-        vim.g.slime_dont_ask_default = 1
-      end
+			local function slime_use_tmux()
+				vim.g.slime_target = "tmux"
+				vim.g.slime_bracketed_paste = 1
+				vim.g.slime_python_ipython = 0
+				vim.g.slime_no_mappings = 1
+				vim.g.slime_default_config = { socket_name = "default", target_pane = ":.2" }
+				vim.g.slime_dont_ask_default = 1
+			end
 
---      local function slime_use_neovim()
---        vim.g.slime_target = "neovim"
---        vim.g.slime_bracketed_paste = 1
---        vim.g.slime_python_ipython = 1
---        vim.g.slime_no_mappings = 1
---        vim.g.slime_get_jobid = slime_get_jobid
---        -- vim.g.slime_default_config = nil
---        -- vim.g.slime_dont_ask_default = 0
---      end
+			--      local function slime_use_neovim()
+			--        vim.g.slime_target = "neovim"
+			--        vim.g.slime_bracketed_paste = 1
+			--        vim.g.slime_python_ipython = 1
+			--        vim.g.slime_no_mappings = 1
+			--        vim.g.slime_get_jobid = slime_get_jobid
+			--        -- vim.g.slime_default_config = nil
+			--        -- vim.g.slime_dont_ask_default = 0
+			--      end
 
-     -- slime_use_neovim()
-       slime_use_tmux()
-      -- }}
-    end
-  },
-
+			-- slime_use_neovim()
+			slime_use_tmux()
+			-- }}
+		end,
+	},
 
 	{ -- requires plugins in lua/plugins/treesitter.lua and lua/plugins/lsp.lua
 		-- for complete functionality (language features)
@@ -85,11 +104,23 @@ return {
 		dev = false,
 		opts = {
 			lspFeatures = {
-				languages = { "r", "python", "julia", "bash", "lua", "html", "dot", "javascript", "typescript", "ojs", "tex"},
+				languages = {
+					"r",
+					"python",
+					"julia",
+					"bash",
+					"lua",
+					"html",
+					"dot",
+					"javascript",
+					"typescript",
+					"ojs",
+					"tex",
+				},
 			},
-      codeRunner = {
-			  default_method = "molten",
-	  	},
+			codeRunner = {
+				default_method = "molten",
+			},
 		},
 		dependencies = {
 			-- for language features in code cells
@@ -119,7 +150,6 @@ return {
 			},
 		},
 	},
-
 
 	{ -- paste an image from the clipboard or drag-and-drop
 		"HakonHarnes/img-clip.nvim",
@@ -157,47 +187,43 @@ return {
 		keys = {
 			{ "<leader>qm", ':lua require"nabla".toggle_virt()<cr>', desc = "toggle math equations" },
 		},
+	},
 
-  },
-
-{
-"benlubas/molten-nvim",
-  enable = true,
-  build = ":UpdateRemotePlugins",
-  init = function()
-      vim.g.molten_open_cmd = "wsl-open" -- "/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
-      vim.g.molten_output_win_max_height = 50
-      vim.g.molten_output_win_max_width = 500
-      vim.g.molten_use_border_highlights = true
-      vim.g.molten_auto_open_output = false
-      vim.g.molten_auto_image_popup = true
-      vim.g.molten_virt_text_output = true
-      vim.g.molten_virt_lines_off_by_1 = true
-      vim.g.molten_auto_open_html_in_browser = false
-  local runner = require("quarto.runner")
-        vim.keymap.set("n", "<leader>mc", runner.run_cell,  { desc = "run cell", silent = true })
-        vim.keymap.set("n", "<leader>ma", runner.run_above, { desc = "run cell and above", silent = true })
-        vim.keymap.set("n", "<leader>mA", runner.run_all,   { desc = "run all cells", silent = true })
-        vim.keymap.set("n", "<leader>mL", runner.run_line,  { desc = "run line", silent = true })
-        vim.keymap.set("v", "<leader>my",  runner.run_range, { desc = "run visual range", silent = true })
-        vim.keymap.set("n", "<leader>mC", function()
-        runner.run_all(true)
-        end, { desc = "run all cells of all languages", silent = true })
-
-
-  end,
-    keys = {
-    {"<leader>mi", ":MoltenInit<cr>", desc = "Init"},
-		{"<leader>mv",	":<C-u>MoltenEvaluateVisual<cr>", mode = "v", desc = "Eval visual",},
-		{"<leader>mr", ":MoltenReevaluateCell<cr>", desc = "Re-eval cell" },
-    {"<leader>mh", ":MoltenHideOutput<cr>", desc = "Hide output"},
-    {"<leader>me", ":MoltenEvaluateOperator<cr>", desc = "Evaluate Operator", silent = true},
-    {"<leader>mq", ":noautocmd MoltenEnterOutput<cr>", desc = "Open Output Window", silent = true},
-    {"<leader>ms", ":MoltenSave<cr>", desc = "Save kernel", silent = true},
-    {"<leader>ml", ":MoltenLoad<cr>", desc = "Load kernel", silent = true},
-    {"<leader>mo", ":!wsl-open output.jpg<cr>", desc = "Open Image", silent = true},
-    {"<leader>md", ":!rm -rf output.jpg<cr>", desc = "Remove Image", silent = true},
-    {"<leader>mb", ":MoltenOpenInBrowser<cr>", desc = "Open in browser", silent = true},
-  },
-},
+	{
+		"benlubas/molten-nvim",
+		enable = true,
+		build = ":UpdateRemotePlugins",
+		init = function()
+			vim.g.molten_open_cmd = "wsl-open" -- "/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
+			vim.g.molten_output_win_max_height = 50
+			vim.g.molten_output_win_max_width = 500
+			vim.g.molten_use_border_highlights = true
+			vim.g.molten_auto_open_output = false
+			vim.g.molten_auto_image_popup = true
+			vim.g.molten_virt_text_output = true
+			vim.g.molten_virt_lines_off_by_1 = true
+			vim.g.molten_auto_open_html_in_browser = false
+			local runner = require("quarto.runner")
+			vim.keymap.set("n", "<leader>mc", runner.run_cell, { desc = "run cell", silent = true })
+			vim.keymap.set("n", "<leader>ma", runner.run_above, { desc = "run cell and above", silent = true })
+			vim.keymap.set("n", "<leader>mA", runner.run_all, { desc = "run all cells", silent = true })
+			vim.keymap.set("n", "<leader>mL", runner.run_line, { desc = "run line", silent = true })
+			vim.keymap.set("v", "<leader>my", runner.run_range, { desc = "run visual range", silent = true })
+			vim.keymap.set("n", "<leader>mC", function()
+				runner.run_all(true)
+			end, { desc = "run all cells of all languages", silent = true })
+		end,
+		keys = {
+			{ "<leader>mi", ":MoltenInit<cr>", desc = "Init" },
+			{ "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "Re-eval cell" },
+			{ "<leader>mh", ":MoltenHideOutput<cr>", desc = "Hide output" },
+			{ "<leader>me", ":MoltenEvaluateOperator<cr>", desc = "Evaluate Operator", silent = true },
+			{ "<leader>mq", ":noautocmd MoltenEnterOutput<cr>", desc = "Open Output Window", silent = true },
+			{ "<leader>ms", ":MoltenSave<cr>", desc = "Save kernel", silent = true },
+			{ "<leader>ml", ":MoltenLoad<cr>", desc = "Load kernel", silent = true },
+			{ "<leader>mo", ":!wsl-open output.jpg<cr>", desc = "Open Image", silent = true },
+			{ "<leader>md", ":!rm -rf output.jpg<cr>", desc = "Remove Image", silent = true },
+			{ "<leader>mb", ":MoltenOpenInBrowser<cr>", desc = "Open in browser", silent = true },
+		},
+	},
 }
