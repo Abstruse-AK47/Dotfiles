10a6c951e7d5e13389e5dd95dd425744ba21c022
Abstruse-AK47 <ayan344kapil@gmail.com>
2025-01-20 18:14:42 +0530

    glazewm

--- .config/nvim/lua/lazy_nvim/plugins/py_env.lua
+++ .config/nvim/lua/lazy_nvim/plugins/py_env.lua
@@ -1,17 +1,18 @@
 return {
 	{
 		"linux-cultist/venv-selector.nvim",
-		cmd = "VenvSelect",
-		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-		opts = {
-			-- Your options go here
-			name = { ".venv", "venv", "tf-gpu" },
-			stay_on_this_version = "true",
-			--	enable_debug_output = true,
-			-- auto_refresh = false
+		enabled = false,
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			"mfussenegger/nvim-dap-python", --optional
+			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 		},
-
-		event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+		lazy = false,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
+		config = function()
+			require("venv-selector").setup()
+		end,
 		keys = {
 			-- Keymap to open VenvSelector to pick a venv.
 			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
@@ -21,24 +22,7 @@ return {
 	},
 	{
 		"kmontocam/nvim-conda",
-		enabled = true,
+		enabled = false,
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 }
-
---return{
---  "linux-cultist/venv-selector.nvim",
---    dependencies = {
---      "neovim/nvim-lspconfig",
---      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
---      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
---    },
---  lazy = false,
---  branch = "regexp", -- This is the regexp branch, use this for the new version
---  config = function()
---      require("venv-selector").setup()
---    end,
---    keys = {
---      { ",v", "<cmd>VenvSelect<cr>" },
---    },
---}
